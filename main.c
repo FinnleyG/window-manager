086dc343d30e6462ecbc83239c23a68a21364bc3
Finnley <finnleygreim@yahoo.com>
2025-10-14 12:10:32 +0200

    added workspaces

--- main.c
+++ main.c
@@ -9,13 +9,18 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+#define STRINGIFY(x) STRINGIFY2(x)
+#define STRINGIFY2(x) #x
+
 #define MOD1 XCB_MOD_MASK_4
 #define MOD2 XCB_MOD_MASK_SHIFT
+
+#define MENUBAR_HEIGHT 30
 
 xcb_connection_t * connection;
 xcb_screen_t * screen;
 xcb_window_t menu_bar; 
-int running = 1;
+uint8_t running = 1;
 int fd;
 
 typedef struct {
@@ -30,46 +35,77 @@ typedef struct {
 	int max_height;
 } Window;
 
-static Window * windows;
-int window_count = 0;
-int active_window = -1;
+#define WORKSPACE_COUNT 9
+
+typedef struct {
+	Window * windows;
+	int window_count;
+	int active_window;
+} Workspace;
+
+Workspace workspaces[WORKSPACE_COUNT];
+int active_workspace = 0;
 
 typedef struct {
     unsigned int mod;
     xcb_keysym_t keysym;
-    void (*func)(void **args);
-    void **args;
+    void (*func)(void * args);
+    void * args;
 } Key;
 
 char * terminalcmd[] = {"alacritty", NULL};
-char * menucmd[] = {"dmenu_run", "-nf", "red", NULL};
-int * pos_one[] = {(int[]){1}};
-int * neg_one[] = {(int[]){-1}};
+char * menucmd[] = {"dmenu_run", "-h", STRINGIFY(MENUBAR_HEIGHT), NULL};
+int pos_one[] = {1};
+int neg_one[] = {-1};
 
-static void closewm(void **args);
-static void launch(void **args);
-static void close_window(void **args);
-static void shift_active_window(void **args);
-static void swap_with(void **args);
+static void testCookie(xcb_void_cookie_t cookie, char *errMessage);
+
+static void move_to_workspace(void * args);
+static void set_active_workspace(void * args);
+static void closewm(void * args);
+static void launch(void * args);
+static void close_window(void * args);
+static void shift_active_window(void * args);
+static void swap_with(void * args);
 
 static Key keys[] = {
-	{ MOD1, 0xff0d, launch, (void**)terminalcmd },
-	{ MOD1, 0x0072, launch, (void**)menucmd },
-	{ MOD1, 0x006b, shift_active_window, (void**)pos_one },
-	{ MOD1, 0x006a, shift_active_window, (void**)neg_one },
-	{ MOD1|MOD2, 0x006b, swap_with, (void**)pos_one },
-	{ MOD1|MOD2, 0x006a, swap_with, (void**)neg_one },
+	{ MOD1, 0xff0d, launch, (void*)terminalcmd },
+	{ MOD1, 0x0072, launch, (void*)menucmd },
+	{ MOD1, 0x006b, shift_active_window, (void*)&pos_one },
+	{ MOD1, 0x006a, shift_active_window, (void*)&neg_one },
+	{ MOD1|MOD2, 0x006b, swap_with, (void*)&pos_one },
+	{ MOD1|MOD2, 0x006a, swap_with, (void*)&neg_one },
 	{ MOD1, 0x0071, close_window, NULL },
-	{ MOD1|MOD2, 0x0071, closewm, NULL }
+	{ MOD1|MOD2, 0x0071, closewm, NULL },
+	{ MOD1, 0x0031, set_active_workspace, (void*)(int[]){0} },
+	{ MOD1, 0x0032, set_active_workspace, (void*)(int[]){1} },
+	{ MOD1, 0x0033, set_active_workspace, (void*)(int[]){2} },
+	{ MOD1, 0x0034, set_active_workspace, (void*)(int[]){3} },
+	{ MOD1, 0x0035, set_active_workspace, (void*)(int[]){4} },
+	{ MOD1, 0x0036, set_active_workspace, (void*)(int[]){5} },
+	{ MOD1, 0x0037, set_active_workspace, (void*)(int[]){6} },
+	{ MOD1, 0x0038, set_active_workspace, (void*)(int[]){7} },
+	{ MOD1, 0x0039, set_active_workspace, (void*)(int[]){8} },
+	{ MOD1|MOD2, 0x0031, move_to_workspace, (void*)(int[]){0} },
+	{ MOD1|MOD2, 0x0032, move_to_workspace, (void*)(int[]){1} },
+	{ MOD1|MOD2, 0x0033, move_to_workspace, (void*)(int[]){2} },
+	{ MOD1|MOD2, 0x0034, move_to_workspace, (void*)(int[]){3} },
+	{ MOD1|MOD2, 0x0035, move_to_workspace, (void*)(int[]){4} },
+	{ MOD1|MOD2, 0x0036, move_to_workspace, (void*)(int[]){5} },
+	{ MOD1|MOD2, 0x0037, move_to_workspace, (void*)(int[]){6} },
+	{ MOD1|MOD2, 0x0038, move_to_workspace, (void*)(int[]){7} },
+	{ MOD1|MOD2, 0x0039, move_to_workspace, (void*)(int[]){8} }
 };
+
 
 uint32_t color_codes[] = { 0x555555, 0x333333, 0x3333FF };
 xcb_gcontext_t * colors = NULL;
 
-static xcb_keycode_t *xcb_get_keycodes(xcb_keysym_t keysym);
+static xcb_keycode_t * xcb_get_keycodes(xcb_keysym_t keysym);
 static xcb_keysym_t xcb_get_keysym(xcb_keycode_t keycode);
 static void handle_key_press(xcb_generic_event_t * ev);
 static void handle_map_request(xcb_generic_event_t * ev);
+static void add_window(Window * window, Workspace * ws);
 static void remove_window(xcb_window_t window);
 static void handle_unmap_notify(xcb_generic_event_t * ev);
 static void handle_destory_notify(xcb_generic_event_t * ev);
@@ -77,12 +113,48 @@ static void setup_colors();
 static void setup();
 static void draw_menubar();
 
-static xcb_gc_t get_font_gc(xcb_window_t window, const char *font_name, uint32_t fg, uint32_t bg);
-static void draw_text(xcb_window_t window, int16_t x1, int16_t y1, const char *label, uint32_t fg, uint32_t bg);
+static xcb_gc_t get_font_gc(xcb_window_t window, const char * font_name, uint32_t fg, uint32_t bg);
+static void draw_text(xcb_window_t window, int16_t x1, int16_t y1, const char * label, uint32_t fg, uint32_t bg);
 
-static void close_window(void **args) {
+static void set_active_workspace(void * args) {
+	if(active_workspace == *(int*)args) return;
+	Workspace * prev_ws = &workspaces[active_workspace];
+	active_workspace = *(int*)args;
+	Workspace * new_ws = &workspaces[active_workspace];
+
+	uint32_t values[] = { 0 };
+	xcb_change_window_attributes_checked(connection, screen->root, XCB_CW_EVENT_MASK, values);
+	for(int i = 0; i < prev_ws->window_count; ++i) {
+		xcb_unmap_window(connection, prev_ws->windows[i].window_id);
+	}
+	values[0] = XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT | XCB_EVENT_MASK_STRUCTURE_NOTIFY | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY | XCB_EVENT_MASK_PROPERTY_CHANGE;
+	xcb_change_window_attributes_checked(connection, screen->root, XCB_CW_EVENT_MASK, values);
+	for(int i = 0; i < new_ws->window_count; ++i) {
+		xcb_map_window(connection, new_ws->windows[i].window_id);
+	}
+	draw_menubar();
+}
+
+static void move_to_workspace(void * args) {
+	if(active_workspace == *(int*)args) return;
+	Workspace * ws = &workspaces[active_workspace];
+	Workspace * target_ws = &workspaces[*(int*)args];
+	Window win = ws->windows[ws->active_window];
+	remove_window(win.window_id);
+	add_window(&win, target_ws);
+	uint32_t values[] = { 0 };
+	xcb_change_window_attributes_checked(connection, screen->root, XCB_CW_EVENT_MASK, values);
+	xcb_unmap_window(connection, win.window_id);
+	values[0] = XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT | XCB_EVENT_MASK_STRUCTURE_NOTIFY | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY | XCB_EVENT_MASK_PROPERTY_CHANGE;
+	xcb_change_window_attributes_checked(connection, screen->root, XCB_CW_EVENT_MASK, values);
+	// dprintf(fd, "%i\n", ws->windows[ws->active_window].window_id);
+	// xcb_unmap_window(connection, ws->windows[ws->active_window].window_id);
+}
+
+static void close_window(void * args) {
 	(void)args;
-	if(window_count > 0) {
+	Workspace * ws = &workspaces[active_workspace];
+	if(ws->window_count > 0) {
 		xcb_client_message_event_t * event = calloc(32, 1);
 
 		const char protocols_atom[] = "WM_PROTOCOLS";
@@ -92,7 +164,7 @@ static void close_window(void **args) {
 		xcb_intern_atom_reply_t * reply_protocols = xcb_intern_atom_reply(connection, cookie_protocols, NULL);
 		xcb_intern_atom_reply_t * reply_delete = xcb_intern_atom_reply(connection, cookie_delete, NULL);
 
-		event->window = windows[active_window].window_id;
+		event->window = ws->windows[ws->active_window].window_id;
 		event->response_type = XCB_CLIENT_MESSAGE;
 		event->format = 32;
 		event->type = reply_protocols->atom;
@@ -108,47 +180,52 @@ static void close_window(void **args) {
 	}
 }
 
-static void shift_active_window(void **args) {
-	active_window = active_window + **(int**)args;
-	if(active_window < 0)
-		active_window = window_count - 1;
-	else if(active_window >= window_count)
-		active_window = 0;
+static void shift_active_window(void * args) {
+	Workspace * ws = &workspaces[active_workspace];
+	if(ws->window_count < 1) return;
+	ws->active_window = ws->active_window + *(int*)args;
+	if(ws->active_window < 0)
+		ws->active_window = ws->window_count - 1;
+	else if(ws->active_window >= ws->window_count)
+		ws->active_window = 0;
 	draw_menubar();
 
-	if(active_window < 0) return;
+	if(ws->active_window < 0) return;
 
 	const uint32_t values[] = { XCB_STACK_MODE_ABOVE };
-	xcb_configure_window(connection, windows[active_window].window_id, XCB_CONFIG_WINDOW_STACK_MODE, values);
+	xcb_configure_window(connection, ws->windows[ws->active_window].window_id, XCB_CONFIG_WINDOW_STACK_MODE, values);
 
-	xcb_set_input_focus(connection, XCB_INPUT_FOCUS_POINTER_ROOT, windows[active_window].window_id, XCB_CURRENT_TIME);
+	xcb_set_input_focus(connection, XCB_INPUT_FOCUS_POINTER_ROOT, ws->windows[ws->active_window].window_id, XCB_CURRENT_TIME);
 }
 
-static void swap_with(void **args) {
-	if(window_count < 2) return;
-	int target_window = active_window + **(int**)args;
+static void swap_with(void * args) {
+	Workspace * ws = &workspaces[active_workspace];
+	if(ws->window_count < 2) return;
+	int target_window = ws->active_window + *(int*)args;
 	if(target_window < 0)
-		target_window = window_count - 1;
-	else if(target_window >= window_count)
+		target_window = ws->window_count - 1;
+	else if(target_window >= ws->window_count)
 		target_window = 0;
-	Window win = windows[target_window];
-	windows[target_window] = windows[active_window];
-	windows[active_window] = win;
+	Window win = ws->windows[target_window];
+	ws->windows[target_window] = ws->windows[ws->active_window];
+	ws->windows[ws->active_window] = win;
 
-	active_window = target_window;
+	ws->active_window = target_window;
 	draw_menubar();
 	const uint32_t values[] = { XCB_STACK_MODE_ABOVE };
-	xcb_configure_window(connection, windows[active_window].window_id, XCB_CONFIG_WINDOW_STACK_MODE, values);
+	xcb_configure_window(connection, ws->windows[ws->active_window].window_id, XCB_CONFIG_WINDOW_STACK_MODE, values);
 
-	xcb_set_input_focus(connection, XCB_INPUT_FOCUS_POINTER_ROOT, windows[active_window].window_id, XCB_CURRENT_TIME);
+	xcb_set_input_focus(connection, XCB_INPUT_FOCUS_POINTER_ROOT, ws->windows[ws->active_window].window_id, XCB_CURRENT_TIME);
 }
 
-static void closewm(void **args) {
+static void closewm(void * args) {
 	(void)args;
 	running = 0;
 }
 
-static void launch(void **command) {
+static void launch(void * args) {
+	char ** command;
+	command = (char**)args;
 	if(fork() == 0) {
 		setsid();
 		if(fork() != 0) {
@@ -177,9 +254,10 @@ static xcb_keysym_t xcb_get_keysym(xcb_keycode_t keycode) {
 }
 
 static void handle_key_press(xcb_generic_event_t * ev) {
+	Workspace * ws = &workspaces[active_workspace];
 	xcb_key_press_event_t * e = (xcb_key_press_event_t*)ev;
 	xcb_keysym_t keysym = xcb_get_keysym(e->detail);
-	if(active_window > -1 && e->child != windows[active_window].window_id) {
+	if(ws->active_window > -1 && e->child != ws->windows[ws->active_window].window_id) {
 		// for(int i = 0; i < window_count; ++i) {
 		// 	if(windows[i].window_id == e->child) {
 		// 		active_window = i;	
@@ -197,12 +275,8 @@ static void handle_key_press(xcb_generic_event_t * ev) {
 	}
 }
 
-#define WINDOW_WIDTH 600
-#define WINDOW_HEIGHT 400
-#define BORDER_WIDTH 0
-#define MENUBAR_HEIGHT 30
-
 static void handle_map_request(xcb_generic_event_t * ev) {
+	Workspace * ws = &workspaces[active_workspace];
 	xcb_map_request_event_t * e = (xcb_map_request_event_t*)ev;
 
 	xcb_get_window_attributes_cookie_t attributes_cookie = xcb_get_window_attributes(connection, e->window);
@@ -267,7 +341,7 @@ static void handle_map_request(xcb_generic_event_t * ev) {
 	values[1] = new_window.y;
 	values[2] = new_window.width;
 	values[3] = new_window.height;
-	values[4] = BORDER_WIDTH;
+	values[4] = 0;
 	xcb_configure_window(connection, e->window, XCB_CONFIG_WINDOW_X |
 		XCB_CONFIG_WINDOW_Y | XCB_CONFIG_WINDOW_WIDTH |
 		XCB_CONFIG_WINDOW_HEIGHT | XCB_CONFIG_WINDOW_BORDER_WIDTH, values);
@@ -280,29 +354,36 @@ static void handle_map_request(xcb_generic_event_t * ev) {
 		xcb_set_input_focus(connection, XCB_INPUT_FOCUS_POINTER_ROOT, e->window, XCB_CURRENT_TIME);
 	}
 
-	active_window = window_count;
-	window_count++;
-	windows = reallocarray(windows, window_count, sizeof(Window));
-	windows[window_count-1] = new_window;
+	add_window(&new_window, ws);
+}
+
+static void add_window(Window * window, Workspace * ws) {
+	ws->active_window = ws->window_count;
+	(ws->window_count)++;
+	ws->windows = reallocarray(ws->windows, ws->window_count, sizeof(Window));
+	ws->windows[ws->window_count-1] = *window;
 }
 
 static void remove_window(xcb_window_t window) {
-	for(int i = 0; i < window_count; ++i) {
-		if(windows[i].window_id == window) {
-			for(int j = i; j < window_count-1; ++j) {
-				windows[j] = windows[j+1];
-			}	
-			window_count--;
-			if(active_window == window_count) {
-				shift_active_window((void**)neg_one);
-			} else {
-				const uint32_t values[] = { XCB_STACK_MODE_ABOVE };
-				xcb_configure_window(connection, windows[active_window].window_id, XCB_CONFIG_WINDOW_STACK_MODE, values);
-				xcb_set_input_focus(connection, XCB_INPUT_FOCUS_POINTER_ROOT, windows[active_window].window_id, XCB_CURRENT_TIME);
+	for(int i = 0; i < WORKSPACE_COUNT; ++i) {
+		Workspace * ws = &workspaces[i];
+		for(int j = 0; j < ws->window_count; ++j) {
+			if(ws->windows[j].window_id == window) {
+				for(int k = j; k < ws->window_count-1; ++k) {
+					ws->windows[k] = ws->windows[k+1];
+				}	
+				(ws->window_count)--;
+				if(ws->active_window == ws->window_count) {
+					shift_active_window((void*)&neg_one);
+				} else {
+					const uint32_t values[] = { XCB_STACK_MODE_ABOVE };
+					xcb_configure_window(connection, ws->windows[ws->active_window].window_id, XCB_CONFIG_WINDOW_STACK_MODE, values);
+					xcb_set_input_focus(connection, XCB_INPUT_FOCUS_POINTER_ROOT, ws->windows[ws->active_window].window_id, XCB_CURRENT_TIME);
+				}
+				ws->windows = reallocarray(ws->windows, ws->window_count, sizeof(Window));
+				draw_menubar();
+				return;
 			}
-			windows = reallocarray(windows, window_count, sizeof(Window));
-			draw_menubar();
-			return;
 		}
 	}
 }
@@ -317,6 +398,25 @@ static void handle_unmap_notify(xcb_generic_event_t * ev) {
 	remove_window(e->window);
 }
 
+// static void handle_client_message(xcb_generic_event_t * ev) {
+	// xcb_client_message_event_t * e = (xcb_client_message_event_t*)ev;
+	// const char atom_type[] = "_NET_WM_STATE";
+	// const char atom_fullscreen[] = "_NET_WM_STATE_FULLSCREEN";
+	// xcb_intern_atom_cookie_t cookie_type =  xcb_intern_atom(connection, 1, strlen(atom_type), atom_type);
+	// xcb_intern_atom_cookie_t cookie_fullscreen =  xcb_intern_atom(connection, 1, strlen(atom_fullscreen), atom_fullscreen);
+	// xcb_intern_atom_reply_t * reply_type = xcb_intern_atom_reply(connection, cookie_type, NULL);
+	// xcb_intern_atom_reply_t * reply_fullscreen = xcb_intern_atom_reply(connection, cookie_fullscreen, NULL);
+	//
+	// xcb_get_atom_name_cookie_t cookie = xcb_get_atom_name(connection, e->data.data32[1]);
+	// xcb_get_atom_name_reply_t * reply = xcb_get_atom_name_reply(connection, cookie, NULL);
+	// char * atomname = xcb_get_atom_name_name(reply);
+	// dprintf(fd, "%s\n", atomname);
+
+	// free(reply_type);
+	// free(reply_fullscreen);
+	// free(reply);
+// }
+
 static void setup_colors() {
 	int color_count = sizeof(color_codes)/sizeof(color_codes[0]);
 	colors = (xcb_gcontext_t*)malloc(color_count * sizeof(xcb_gcontext_t));
@@ -330,14 +430,10 @@ static void setup_colors() {
 
 static void setup() {
 	uint32_t values[1];
-	values[0] = XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT
-		| XCB_EVENT_MASK_STRUCTURE_NOTIFY
-		| XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY
-		| XCB_EVENT_MASK_PROPERTY_CHANGE;
+	values[0] = XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT | XCB_EVENT_MASK_STRUCTURE_NOTIFY | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY | XCB_EVENT_MASK_PROPERTY_CHANGE;
 		// | XCB_EVENT_MASK_ENTER_WINDOW
 		// | XCB_EVENT_MASK_LEAVE_WINDOW;
-	xcb_change_window_attributes_checked(connection, screen->root,
-		XCB_CW_EVENT_MASK, values);
+	xcb_change_window_attributes_checked(connection, screen->root, XCB_CW_EVENT_MASK, values);
 	xcb_ungrab_key(connection, XCB_GRAB_ANY, screen->root, XCB_MOD_MASK_ANY);
 	int key_table_size = sizeof(keys) / sizeof(*keys);
 	for (int i = 0; i < key_table_size; ++i) {
@@ -355,6 +451,11 @@ static void setup() {
 		XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC,
 		XCB_GRAB_MODE_ASYNC, screen->root, XCB_NONE, 3, MOD1);
 	xcb_flush(connection);
+
+	for(int i = 0; i < WORKSPACE_COUNT; ++i) {
+		workspaces[i].window_count = 0;
+		workspaces[i].active_window = -1;
+	}
 }
 
 static void testCookie(xcb_void_cookie_t cookie, char *errMessage) {
@@ -400,25 +501,40 @@ static xcb_gc_t get_font_gc(xcb_window_t window, const char *font_name, uint32_t
 	return gc;
 }
 
+#define WORKSPACE_WIDTH 20
 static void draw_menubar() {
 	xcb_clear_area(connection, 0, menu_bar, 0, 0, screen->width_in_pixels, MENUBAR_HEIGHT);
 
-	// char str[10];
-	// sprintf(str, "%i, %i", window_count, active_window);
-	// draw_text(menu_bar, screen->width_in_pixels-40, MENUBAR_HEIGHT - 10, str, screen->white_pixel, color_codes[0]);
-	if(window_count == 0) return;
-
-	int tab_width = (screen->width_in_pixels)/window_count;
-
 	int col;
-	for(int i = 0; i < window_count; ++i) {
-		xcb_rectangle_t rect = { tab_width*i, 0, tab_width, MENUBAR_HEIGHT };
-		if(i == active_window)
+	for(int i = 0; i < WORKSPACE_COUNT; ++i) {
+		if(i == active_workspace)
+			col = 2;
+		else
+			col = 0;
+		xcb_rectangle_t rect = { WORKSPACE_WIDTH*i, 0, WORKSPACE_WIDTH, MENUBAR_HEIGHT };
+		xcb_poly_fill_rectangle(connection, menu_bar, colors[col], 1, &rect);
+		char num[2];
+		sprintf(num, "%i", i+1);
+		draw_text(menu_bar, WORKSPACE_WIDTH/2+WORKSPACE_WIDTH*i, MENUBAR_HEIGHT - 10, num, screen->white_pixel, color_codes[col]);
+		if(workspaces[i].window_count > 0) {
+			xcb_rectangle_t rect2 = { WORKSPACE_WIDTH*i, 0, 5, 5 };
+			xcb_poly_fill_rectangle(connection, menu_bar, colors[2], 1, &rect2);
+		}
+	}
+
+	Workspace * ws = &workspaces[active_workspace];
+	if(ws->window_count == 0) return;
+
+	int tab_width = (screen->width_in_pixels-WORKSPACE_WIDTH*WORKSPACE_COUNT)/ws->window_count;
+
+	for(int i = 0; i < ws->window_count; ++i) {
+		xcb_rectangle_t rect = { tab_width*i+WORKSPACE_WIDTH*WORKSPACE_COUNT, 0, tab_width, MENUBAR_HEIGHT };
+		if(i == ws->active_window)
 			col = 2;
 		else
 			col = i%2;
 		xcb_poly_fill_rectangle(connection, menu_bar, colors[col], 1, &rect);
-		draw_text(menu_bar, 30+tab_width*i, MENUBAR_HEIGHT - 10, windows[i].name, screen->white_pixel, color_codes[col]);
+		draw_text(menu_bar, 30+tab_width*i+WORKSPACE_WIDTH*WORKSPACE_COUNT, MENUBAR_HEIGHT - 10, ws->windows[i].name, screen->white_pixel, color_codes[col]);
 	}
 	xcb_flush(connection);
 }
@@ -466,7 +582,7 @@ int main() {
 			handle_unmap_notify(ev);
 		}
 		// if ((ev->response_type & ~0x80) == XCB_CLIENT_MESSAGE) {
-		//
+		// 	handle_client_message(ev);
 		// }
 		free(ev);
 		xcb_flush(connection);
